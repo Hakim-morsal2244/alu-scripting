77f59f8d0c12d10c74ec7ebbc40389310da9919f
Hakim-morsal2244 <m.hakim@alustudent.com>
2025-02-20 16:55:33 +0000

    Update script

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,18 @@
+#!/usr/bin/python3
+"""DOCS"""
 import requests
 
+
 def top_ten(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    
-    # Headers to avoid 403 error (Reddit API requires a User-Agent)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    
-    # Make the request and prevent redirection
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    # Check if the response is successful (status code 200)
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
+
     if response.status_code == 200:
-        # Get the list of posts from the JSON response
-        posts = response.json().get('data', {}).get('children', [])
-        
-        # Print the titles of the first 10 posts
-        for post in posts[:10]:
+        data = response.json()['data']
+        for post in data['children'][:10]:
             print(post['data']['title'])
     else:
-        # If subreddit is invalid or not found, print None
         print(None)
