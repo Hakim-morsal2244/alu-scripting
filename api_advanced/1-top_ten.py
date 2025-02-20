0841ca96073e08f307938dbede4fe3c69edd4dc5
Hakim-morsal2244 <m.hakim@alustudent.com>
2025-02-20 16:29:05 +0000

    Task 2

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,32 +1,22 @@
-#!/usr/bin/python3
-
-"""
-Displays the titles of 10 hot posts listed for a subreddit
-"""
-
-from requests import get
-import subprocess
-
+import requests
 
 def top_ten(subreddit):
-    """
-    The Function that fethces the Reddit API
-    """
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    if response.status_code != 200:
-        print("None")
-        return
-
-    try:
-        results = response.json()
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
-        print("None")
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    
+    # Headers to avoid 403 error (Reddit API requires a User-Agent)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    # Make the request and prevent redirection
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    # Check if the response is successful (status code 200)
+    if response.status_code == 200:
+        # Get the list of posts from the JSON response
+        posts = response.json().get('data', {}).get('children', [])
+        
+        # Print the titles of the first 10 posts
+        for post in posts[:10]:
+            print(post['data']['title'])
+    else:
+        # If subreddit is invalid or not found, print None
+        print(None)
